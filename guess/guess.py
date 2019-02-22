3ef05bd167776c89b5e0375e8a5ac89130fbcde1
Shane Claussen <sclaussen@yahoo.com>
2019-02-22 11:26:13 -0800

    Remove newline when prompting the user for name and guess

--- guess/guess.py
+++ guess/guess.py
@@ -2,24 +2,21 @@ import random
 
 guessCount = 0
 
-print('Hello!  What is your name?')
+print('Hey there!!!  What is your name?', end=' ')
 name = input();
 
 number = random.randint(1, 25)
 
 for guessCount in range(6):
-    print('Take a guess: ')
-    guess = input()
-    guess = int(guess)
+    print('Take a guess: ', end='')
+    guessString = input()
+    guess = int(guessString)
 
     if guess == number:
         break
 
     if guess < number:
         print('Your guess is too low.')
-
-    if guess > number:
-        print('Your guess is too high.')
 
 if guess == number:
     guessCount = str(guesCount + 1)
